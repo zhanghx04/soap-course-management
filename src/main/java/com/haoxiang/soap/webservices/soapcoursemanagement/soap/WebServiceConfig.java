627a7000ea4b67001becf10ce1eae1c1ed7e232d
Haoxiang Zhang <zhanghx04@gmail.com>
2023-04-08 15:11:17 -0500

    Implemented security for SOAP web service with WS Security

--- src/main/java/com/haoxiang/soap/webservices/soapcoursemanagement/soap/WebServiceConfig.java
+++ src/main/java/com/haoxiang/soap/webservices/soapcoursemanagement/soap/WebServiceConfig.java
@@ -6,27 +6,34 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.ws.config.annotation.EnableWs;
+import org.springframework.ws.config.annotation.WsConfigurerAdapter;
+import org.springframework.ws.server.EndpointInterceptor;
+import org.springframework.ws.soap.security.xwss.XwsSecurityInterceptor;
+import org.springframework.ws.soap.security.xwss.callback.SimplePasswordValidationCallbackHandler;
 import org.springframework.ws.transport.http.MessageDispatcherServlet;
 import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
 import org.springframework.xml.xsd.SimpleXsdSchema;
 import org.springframework.xml.xsd.XsdSchema;
 
+import java.util.Collections;
+import java.util.List;
+
 // Enable Spring Web Services
 @EnableWs
 // Spring Configuration
 @Configuration
-public class WebServiceConfig {
+public class WebServiceConfig extends WsConfigurerAdapter {
     // MessageDispatcherServlet
         // ApplicationContext
     // url -> /ws/*
     @Bean
-    public ServletRegistrationBean messageDispatcherServlet(ApplicationContext context) {
+    public ServletRegistrationBean<MessageDispatcherServlet> messageDispatcherServlet(ApplicationContext context) {
         MessageDispatcherServlet messageDispatcherServlet = new MessageDispatcherServlet();
         messageDispatcherServlet.setApplicationContext(context);
         // Create WSDL
         messageDispatcherServlet.setTransformWsdlLocations(true);
 
-        return new ServletRegistrationBean(messageDispatcherServlet, "/ws/*");
+        return new ServletRegistrationBean<>(messageDispatcherServlet, "/ws/*");
     }
 
     // Create WSDL
@@ -49,5 +56,32 @@ public class WebServiceConfig {
     @Bean
     public XsdSchema coursesSchema() {
         return new SimpleXsdSchema(new ClassPathResource("course-details.xsd"));
+    }
+
+
+    // XwsSecurityInterceptor
+        // Callback Handler -> SimplePasswordValidationCallbackHandler
+        // Security Policy -> securityPolicy.xml
+    // Interceptors.add -> XwsSecurityInterceptor
+    @Bean
+    public XwsSecurityInterceptor securityInterceptor() {
+        XwsSecurityInterceptor securityInterceptor = new XwsSecurityInterceptor();
+        securityInterceptor.setCallbackHandler(callbackHandler());
+        securityInterceptor.setPolicyConfiguration(new ClassPathResource("securityPolicy.xml"));
+
+        return securityInterceptor;
+    }
+
+    @Bean
+    public SimplePasswordValidationCallbackHandler callbackHandler() {
+        SimplePasswordValidationCallbackHandler handler = new SimplePasswordValidationCallbackHandler();
+        handler.setUsersMap(Collections.singletonMap("user", "pass"));
+
+        return handler;
+    }
+
+    @Override
+    public void addInterceptors(List<EndpointInterceptor> interceptors) {
+        interceptors.add(securityInterceptor());
     }
 }
